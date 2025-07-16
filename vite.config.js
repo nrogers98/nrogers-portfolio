a674b7c2e11bc9c10f6d99c5c652841acb3da96a
nrogers98 <rogersnoah45@gmail.com>
2025-07-16 15:33:21 -0300

    ok

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/noah-portfolio",
+  base: "/nrogers98.github.io/",
   resolve: {
     alias: {
       // eslint-disable-next-line no-undef
