4e5d1b7edae1d7212879fcfbfec09e5d470e7cda
nrogers98 <rogersnoah45@gmail.com>
2025-07-19 04:56:12 -0300

    jk

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,12 +4,12 @@ import { Navbar } from "@/components/Navbar.jsx"
 import { HomeSection } from "../components/HomeSection.jsx";
 import { AboutSection } from "../components/AboutSection.jsx";
 import { SkillsSection } from "../components/SkillsSection.jsx";
-import { ProjectSection } from "../components/ProjecstSection.jsx";
+import { ProjectSection } from "../components/ProjectsSection.jsx";
 import { ContactSection } from "../components/ContactSection.jsx";
 import { Footer } from "../components/Footer.jsx";
 
 export const Home = () => {
-    return <div className="min-h-screen bg-background text-foreground overflow-x-hiddn">
+    return <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
         {/* Theme Toggle */}
         <ThemeToggle />
         {/* Background Effects */}
