b86e3943950ebd787662a236975d1a4d6e91f4e2
nrogers98 <rogersnoah45@gmail.com>
2025-07-21 02:44:20 -0300

    basename

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <section className="App">
       <Toaster />
-      <HashRouter basename={import.meta.env.BASE_URL}>
+      <HashRouter basename="/">
         <Routes>
           <Route path="/" element={<Home />}/>
           <Route path="*" element={<NotFound />}/>
