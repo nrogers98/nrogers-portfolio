d1204df03a4a451050fa0cfb1b1a053aa653f4e6
nrogers98 <rogersnoah45@gmail.com>
2025-07-21 01:24:35 -0300

    base

--- vite.config.js
+++ vite.config.js
@@ -3,22 +3,14 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import path from "path";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  const config = {
-    plugins: [react(), tailwindcss()],
-    base: '/',
-    resolve: {
-      alias: {
-        // eslint-disable-next-line no-undef
-        "@": path.resolve(__dirname, "./src")
-      },
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  base: "/nrogers-portfolio/",
+  resolve: {
+    alias: {
+      // eslint-disable-next-line no-undef
+      "@": path.resolve(__dirname, "./src")
     },
-  }
-
-  if (command !== 'serve') {
-    config.base = '/nrogers-portfolio/'
-  }
-
-  return config
-})
+  },
+});
