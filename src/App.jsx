67bf253058b263a1115b90d76e54c07160665c02
nrogers98 <rogersnoah45@gmail.com>
2025-07-21 02:50:40 -0300

    it wont work, i'm going to deploy this 100000000000 times :(

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <section className="App">
       <Toaster />
-      <HashRouter basename="/nrogers-portfolio/">
+      <HashRouter basename="/">
         <Routes>
           <Route path="/" element={<Home />}/>
           <Route path="*" element={<NotFound />}/>
